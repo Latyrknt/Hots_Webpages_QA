8c55754f11d489fdc448c0faf419d619a394b29b
LatyrT <60825191+LatyrT@users.noreply.github.com>
2022-05-16 17:42:23 -0400

    global digital divide

---
+++ Project Work/02 - Analysis/04_Flask_SQLAchemy/readcsv.py
@@ -0,0 +1,216 @@
+import json
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import Column, Integer, String, Float
+
+
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+
+
+from flask import Flask, jsonify
+
+url = 'postgresql://{user}:{pw}@{url}/{db}'.format(user="postgres",pw="postgres",url="localhost",db="Internet_DB")
+
+
+
+
+
+class Country_Data(Base):
+  __tablename__ = "country"
+  index = Column(Integer, primary_key=True)
+  countries = Column(String)
+  internet_users = Column(String)
+  penetration = Column(String)
+  population = Column(String)
+  region = Column(String)
+  longitude = Column(String)
+  latitude = Column(String)
+
+
+class webindex(Base):
+  __tablename__ = "wi"
+  index = Column(Integer, primary_key=True)
+  countries = Column(String)
+  overall_score = Column(sqlalchemy.types.Float)
+  longitude = Column(sqlalchemy.types.Float)
+  latitude = Column(sqlalchemy.types.Float)
+  universal_access = Column(sqlalchemy.types.Float)
+  freedom_openness = Column(sqlalchemy.types.Float)
+  relevant_content = Column(sqlalchemy.types.Float)
+  empowerment = Column(sqlalchemy.types.Float)
+  
+class World_Internet(Base):
+  __tablename__ = "world"
+  index = Column(Integer, primary_key=True)
+  world_regions = Column(String)
+  population = Column(String)
+  pop = Column(String)
+  internet_users = Column(String)
+  penetration_rate = Column(String)
+  growth = Column(String)
+  internet_world = Column(String)
+
+class Cost_Speed(Base):
+  __tablename__ = "cost_speed"
+  index = Column(Integer, primary_key=True)
+  countries = Column(String)
+  avg_monthly_cost_in_gbp = Column(sqlalchemy.types.Float)
+  avg_download_speed_mbps = Column(sqlalchemy.types.Float)
+  avg_upload_speed_mbps = Column(sqlalchemy.types.Float)
+
+ 
+
+engine = create_engine(url)
+Base.metadata.create_all(engine)
+
+
+
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/InternetByCountry<br/>"
+        f"/api/v1.0/WebIndex1<br/>"
+        f"/api/v1.0/WebIndex2<br/>"
+        f"/api/v1.0/WorldInternet<br/>"
+        f"/api/v1.0/CostandSpeed<br/>"
+    )
+
+
+@app.route("/api/v1.0/InternetByCountry")
+#Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+def name():
+    #Query result
+    session = Session(bind=engine)
+    Country_list=session.query(Country_Data.countries,Country_Data.internet_users,Country_Data.penetration,Country_Data.population,Country_Data.region,Country_Data.longitude,Country_Data.latitude).all()
+    session.close()
+    print(Country_Data)
+    print(Country_list)
+    #Create list containing information
+    country_dict=[]
+    for i in range(len( Country_list)):
+        percentage=Country_list[i][2][0:len(Country_list[i][2])-2]
+        country_dict.append({
+            "Country": Country_list[i][0],
+            "InternetUsers":int(Country_list[i][1]),
+            "Percent_Population":int(round(float(percentage),0)),
+            "Population":int(Country_list[i][3]),
+            "Region":Country_list[i][4],
+            "Location":[Country_list[i][6],Country_list[i][5]]
+        })
+    #Return the JSON representation of your dictionary.
+    return jsonify(country_dict)
+
+
+@app.route("/api/v1.0/WebIndex1")
+def internet_index1():
+    #Query result
+    session = Session(bind=engine)
+    web_index_list1=session.query(webindex.countries,webindex.overall_score,webindex.longitude,webindex.latitude,webindex.universal_access,webindex.freedom_openness,webindex.relevant_content,webindex.empowerment).all()
+    session.close()
+    print(webindex)
+    print(web_index_list1)
+    #Create list containing information
+    web_index_dict1=[]
+    for i in range(len(web_index_list1)):
+        web_index_dict1.append({
+            "Countries": web_index_list1[i][0],
+            "Latitude":float(web_index_list1[i][3]),
+            "Longitude":float(web_index_list1[i][2]),
+            "Overall score":web_index_list1[i][1],
+            "Universal Access":web_index_list1[i][4],
+            "Freedoms & Openness":web_index_list1[i][5],
+            "Relevant Content":web_index_list1[i][6],
+            "Empowerment":web_index_list1[i][7]
+        })
+    #Return the JSON representation of your dictionary.
+    return jsonify(web_index_dict1)
+
+
+@app.route("/api/v1.0/WebIndex2")
+def internet_index2():
+    #Query result
+    session = Session(bind=engine)
+    web_index_list2=session.query(webindex.countries,webindex.overall_score,webindex.longitude,webindex.latitude,webindex.universal_access,webindex.freedom_openness,webindex.relevant_content,webindex.empowerment).all()
+    session.close()
+    print(webindex)
+    print(web_index_list2)
+    #Create list containing information
+    web_index_dict2=[]
+    for i in range(len(web_index_list2)):
+        web_index_dict2.append({
+            "countries": web_index_list2[i][0],
+            "overalls":web_index_list2[i][1],
+            "universals":web_index_list2[i][4],
+            "freedoms":web_index_list2[i][5],
+            "relevant_content":web_index_list2[i][6],
+            "empowerment":web_index_list2[i][7],
+            "location":[float(web_index_list2[i][3]),float(web_index_list2[i][2])]
+        })
+    #Return the JSON representation of your dictionary.
+    return jsonify(web_index_dict2)
+
+@app.route("/api/v1.0/WorldInternet")
+def world_internet():
+    #Query result
+    session = Session(bind=engine)
+    wrold_internet_stats=session.query(World_Internet.world_regions,World_Internet.population,World_Internet.pop,World_Internet.internet_users,World_Internet.penetration_rate,World_Internet.growth,World_Internet.internet_world).all()
+    session.close()
+    print(World_Internet)
+    print(wrold_internet_stats)
+    #Create list containing information
+    world_web_dict=[]
+    for i in range(len(wrold_internet_stats)-1):
+        Growth=wrold_internet_stats[i][5][0:len(wrold_internet_stats[i][5])-2]
+        print(Growth)
+        Growth1=Growth.split(",")
+        print(Growth.split(","))
+        Growth2=''.join(Growth1)
+        print(Growth2)
+        world_web_dict.append({
+            "World_Regions": wrold_internet_stats[i][0],
+            "Population":wrold_internet_stats[i][1],
+            "Population_Perc_of_World":float(wrold_internet_stats[i][2][0:len(wrold_internet_stats[i][2])-2])/100,
+            "Internet_Users":wrold_internet_stats[i][3],
+            "Penetration_Rate":float(wrold_internet_stats[i][4][0:len(wrold_internet_stats[i][4])-2])/100,
+            "Growth_2000_2020":float(Growth2)/100,
+            "Internet_World_Perc":float(wrold_internet_stats[i][6][0:len(wrold_internet_stats[i][6])-2])/100
+        })
+    #Return the JSON representation of your dictionary.
+    return  jsonify(world_web_dict)
+
+
+@app.route("/api/v1.0/CostandSpeed")
+def costAndspeed():
+    #Query result
+    session = Session(bind=engine)
+    cost_speed_stats=session.query(Cost_Speed.countries,Cost_Speed.avg_monthly_cost_in_gbp,Cost_Speed.avg_download_speed_mbps).all()
+    session.close()
+    print(Cost_Speed)
+    print(cost_speed_stats)
+    #Create list containing information
+    cost_speed_dict=[]
+    for i in range(len(cost_speed_stats)):
+        cost_speed_dict.append({
+            "country": cost_speed_stats[i][0],
+            "cost":cost_speed_stats[i][1],
+            "dlspeed":cost_speed_stats[i][2]
+        })
+    #Return the JSON representation of your dictionary.
+    return  jsonify(cost_speed_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# javascript-challenge
+# **Global Digital Divide Project 2**
+
+In this assignment, the objective is to create an end-to-end ETL (Extract, Transform, Load) process.
+All data will be rendered on a website using charts, maps and tables.
+See more details in the requirements/specifications section below.
+
+<p align="center">
+  <img src="Gs3jCL2.png">
+</p>  
+
+## **1. Authors**
+
+This project was created and authored by:
+* **Anna Wrobel**
+* **Latyr Thiao**
+* **Lujane Abdelgadir**
+* **Prajakta Galvankar**
+* **Stephen Zhang**
+
+
+## **2. Requirements / Specifications**
+
+The business & technical requirements that must be met by the solution:
+* [Project 2](01_Project2.pdf)
+* [Diagram](01_Diagram.pdf)
+
+## **3. Prerequisites**
+
+The mandatory steps to be taken to make the code work smoothly.
+
+### **3.1 License & Key**
+
+You must have set up a password to connect to a database. See details [here](03_User_Guide_v0.3.docx).
+
+
+### **3.2 Installing**
+
+The assumption here is that your computer is running on Windows 10.
+Feel free to adjust if you are using a Mac OS or any other operating system.
+
+What software do you need and how to install them?
+
+* Jupyter Notebook
+* All Python modules required to run the process. See details [here](03_User_Guide_v0.3.docx).
+* Any web browser (Chrome, ...) 
+* Anaconda/Python terminal
+* Any IDE that enable to write or read code (VS Code, ...)
+
+
+## **4. Tools & Coding Languages**
+
+The following tools and coding languages were used:
+* HTML
+* JavaScript
+* Css
+* Flask
+* pgAdmin (SQL)
+
+
+## **5. Data Source(s)**
+
+
+The data used as input is coming from the following sources:
+* World bank API: https://datahelpdesk.worldbank.org/knowledgebase/articles/898581-api-basic-call-structures
+* Web index: https://thewebindex.org/downloads/
+* Compare the market: https://www.comparethemarket.com/broadband/content/global-broadband-index/
+* Internet World Stats: https://www.internetworldstats.com/stats1.html
+
+NOTE: The data sources websites may have evolved, causing the scrping and Jupyter Notebook not to work a year after this project. 
+
+## **6. Run the code**
+
+### **Step 1: The code is located here**
+Follow [this documentation](03_User_Guide_v0.3.docx) to see what code to run.
+Subsequent code, e.g. the destination html page to open after the index page, is available but not described in the documentation.
+
+### **Step 2: Extract all files & run the code**
+* Download this entire project from Github
+* Open VS Code or your IDE
+* Choose 'File > Open Folder'
+* Select the path to the folder located 1 level above the folder where the [HTML](index.html) is located. 
+    * Example if your index file is located: Documents/.../Folder_Level_1_Above/17_Global_Digital_Divide_Project_2
+    * Select: Folder_Level_1_Above
+* Right click the folder that contains your [HTML](index.html), it should be named: 17_Global_Digital_Divide_Project_2
+* Select: Open in integrated terminal
+* It should open the terminal
+* In the terminal do the following:
+    * Type the command 'conda activate Your_Environment_Name'
+    * Your_Environment_Name is the name of your Python environment you defined
+    * Run the server by typing the following: 'Python -m http.server'
+    * Then, copy/paste this url [localhost](http://localhost:8000/) into your browser and press enter
+* The page should load the welcome page and you can navigate to show the desired information
+* Don't forget to shut down the server by closing your Python terminal in VS Code
+
+
+## **7. User Acceptance Testing**
+
+See related documentation to learn more about acceptence criteria:  
+* [Project 2](01_Project2.pdf)
+* [Diagram](01_Diagram.pdf)
+
+## **8. Deployment**
+
+* You could also deploy this entire folder with your preferred service provider (Github, ...) where a website can be hosted
+* See [how to host a website on Github](https://www.youtube.com/watch?v=M5mg0r4ajt4&list=TLPQMTgwODIwMjFMJ3NIh8pJ7w&index=2)
+
+
+## **9. Expected result**
+
+NOTE: The data sources websites may have evolved, causing the scrping and Jupyter Notebook not to work a year after this project.
+
+* [Documentation](03_User_Guide_v0.3.docx)
+* [Presentation](04_Presentation.pdf)
+
+The outcome should be similar to the below:
+
+<p align="center">
+  <img src="zwelcome_page.png">
+</p> 
+
+<p align="center">
+  <img src="zglobal.png">
+</p> 
+
+<p align="center">
+  <img src="zregional.png">
+</p>
+
+<p align="center">
+  <img src="ztable.png">
+</p>
+
+
